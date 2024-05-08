e4c39d47e443629adc7c80935d4b3e78277276cb
amancoder17 <aman.panwar.cs.2019@miet.ac.in>
2024-05-08 11:06:03 +0530

    Aman commited

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Keycloak from 'keycloak-js';
+import Nav from "./components/Nav";
+import Protected from './components/Protected';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+const keycloak = new Keycloak({
+    realm: 'performance',
+    url: 'http://localhost:8080',
+    clientId: 'performclient'
+});
+
+keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
+    if (authenticated) {
+        console.log('User is authenticated');
+    } else {
+        console.log('User is not authenticated');
+    }
+});
+
+const App = () => { 
+ return  <div>
+  <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<Nav/>}/>
+  <Route path="/public" element={<Protected/>}/>
+  </Routes>
+  </BrowserRouter>
+  
+ 
+ </div>
+}
+export default App
